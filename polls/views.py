6e2ccb117962a99b475d53108bfecf6474c985e7
Gabriel Perez <gapc87@gmail.com>
2020-05-09 18:37:08 +0200

    sobreescrita la avista index

--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse('Hello world!')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
