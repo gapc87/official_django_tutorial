c1baa7f576853295d2c163468161456e75127ad4
Gabriel Perez <gapc87@gmail.com>
2020-05-09 18:57:22 +0200

    vista detail refactorizada y nuevo template para dicha vista

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from polls.models import Question
@@ -12,8 +12,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
